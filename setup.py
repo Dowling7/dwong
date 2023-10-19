91cb768f844ea370768b1789f03a54cc6ef3f3c3
Dowling_Wong <80257366+Dowling7@users.noreply.github.com>
2023-10-19 02:40:48 -0400

    added description and samples

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
+        'numpy>=1.18.0',              
+        'keras == 2.13.1',              
+        'uproot >= 5.0.7',           
+        'sklearn >= 1.2.2',             
        
     ],
     author='Dowling Wong',
-    author_email='your.email@example.com',
-    description='A brief description of your package',
+    author_email='dowlingwong@brandeis.edu',
+    description='Dowling\'s integrated data analysis for DQ experiemnt setup. Include data analysis functions, csv saving tools, Particle ID model, DNN model frame for ID training and EMCal ploting tools',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/mypackage',
+    url='https://github.com/Dowling7/dwong',
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
